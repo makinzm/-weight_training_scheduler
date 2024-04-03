2c3af4de46fce2743a1bf538dde1de314fe32119
makinzm <yuruseba@yahoo.co.jp>
2024-04-03 09:35:47 +0900

    feat: 🎸 add Presenter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'presentation/page/count_up_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,81 +19,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  static const incrementButtonX = -0.9;
-  static const incrementButtonY = 1.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Stack(
-        children: <Widget>[
-          Align(
-            alignment: const Alignment(incrementButtonX, incrementButtonY),
-            child: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: FloatingActionButton.extended(
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              label: const Text(
-                '-',
-                style: TextStyle(height: 40, fontSize: 40),
-              ),
-            )
-          )
-      ],
-    ),
-    );    // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
